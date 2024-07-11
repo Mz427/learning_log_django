c67320097167f3fef416244c7c1f4f84bf774eb6
MaZhuang <zhuangzhimhao@outlook.com>
2024-07-11 11:28:05 +0800

    modified:   python_django_NFSs/__pycache__/models.cpython-312.pyc 	modified:   python_django_NFSs/__pycache__/views.cpython-312.pyc 	new file:   python_django_NFSs/migrations/0003_delete_modelwithfilefield.py 	new file:   python_django_NFSs/migrations/__pycache__/0003_delete_modelwithfilefield.cpython-312.pyc 	modified:   python_django_NFSs/models.py 	modified:   python_django_NFSs/templates/python_django_NFSs/list_files.html 	modified:   python_django_NFSs/views.py

--- python_django_NFSs/models.py
+++ python_django_NFSs/models.py
@@ -1,5 +1,3 @@
 from django.db import models
 
 # Create your models here.
-class ModelWithFileField(models.Model):
-    path = models.FileField(upload_to=".")
--- python_django_NFSs/views.py
+++ python_django_NFSs/views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from .models import ModelWithFileField
 import os
 
 # Create your views here.
@@ -8,16 +7,19 @@ def index(request):
     return render(request, 'python_django_NFSs/index.html')
 
 def list_files(request, dir_name):
+    dir_path = f"/home/mz/{dir_name}/"
     if request.method == "GET":
-        dir_path = f"/home/mz/{dir_name}/"
         files_attribute = {}
         for i in os.listdir(dir_path):
             if os.path.isdir(dir_path + i):
                 files_attribute[i] = "dir"
             else:
                 files_attribute[i] = "file"
-        return render(request, "python_django_NFSs/list_files.html", {"files_attribute": files_attribute})
+        return render(request, "python_django_NFSs/list_files.html", {"files_attribute": files_attribute, "cwd": dir_name + "/"})
     elif request.method == "POST":
-        upload_files = request.FILES.get("upload_files")
-        ModelWithFileField(path=upload_files).save()
-        return HttpResponse("Success!")
+        print(request.POST)
+        upload_files = request.FILES["upload_files"]
+        with open(f"{dir_path}{upload_files.name}", "wb+") as destination:
+            for chunk in upload_files.chunks():
+                destination.write(chunk)
+        return HttpResponse("The file upload successfully!")
