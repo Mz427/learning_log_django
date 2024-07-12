c703051429d210d89014af875d5b849781d5f0fe
MaZhuang <zhuangzhimhao@outlook.com>
2024-07-12 10:28:33 +0800

    modified:   python_django_NFSs/__pycache__/urls.cpython-312.pyc 	modified:   python_django_NFSs/__pycache__/views.cpython-312.pyc 	renamed:    python_django_NFSs/templates/python_django_NFSs/list_files.html -> python_django_NFSs/templates/python_django_NFSs/files_management.html 	modified:   python_django_NFSs/urls.py 	modified:   python_django_NFSs/views.py

--- python_django_NFSs/urls.py
+++ python_django_NFSs/urls.py
@@ -4,5 +4,5 @@ from . import views
 app_name = 'python_django_NFSs'
 urlpatterns = [
     path('', views.index, name='index'),
-    path('storage/<path:dir_name>/', views.list_files, name='list_files'),
+    path('storage/<path:dir_name>/', views.files_management, name='manage_files'),
 ]
--- python_django_NFSs/views.py
+++ python_django_NFSs/views.py
@@ -1,12 +1,12 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, FileResponse
 import os
 
 # Create your views here.
 def index(request):
     return render(request, 'python_django_NFSs/index.html')
 
-def list_files(request, dir_name):
+def files_management(request, dir_name):
     dir_path = f"/home/mz/{dir_name}/"
     if request.method == "GET":
         files_attribute = {}
@@ -15,11 +15,23 @@ def list_files(request, dir_name):
                 files_attribute[i] = "dir"
             else:
                 files_attribute[i] = "file"
-        return render(request, "python_django_NFSs/list_files.html", {"files_attribute": files_attribute, "cwd": dir_name + "/"})
+        return render(request, "python_django_NFSs/files_management.html", {"files_attribute": files_attribute, "cwd": dir_name + "/"})
     elif request.method == "POST":
-        print(request.POST)
-        upload_files = request.FILES["upload_files"]
-        with open(f"{dir_path}{upload_files.name}", "wb+") as destination:
-            for chunk in upload_files.chunks():
-                destination.write(chunk)
-        return HttpResponse("The file upload successfully!")
+        if request.POST["submit"] == "Download":
+            for i, j in request.POST.items():
+                if j in ("file", "dir"):
+                    if i != "":
+                        print(i)
+                        # with open(f"{dir_path}{i}", "rb") as file_downloaded:
+                        file_downloaded = open(f"{dir_path}{i}", "rb") 
+                        return FileResponse(file_downloaded, as_attachment=True)
+        elif request.POST["submit"] == "Upload":
+            upload_files = request.FILES["upload_files"]
+            with open(f"{dir_path}{upload_files.name}", "wb+") as file_uploaded:
+                for chunk in upload_files.chunks():
+                    file_uploaded.write(chunk)
+            return HttpResponse("The file upload successfully!")
+        elif request.POST["submit"] == "Delete":
+            return HttpResponse("The file Delete successfully!")
+        elif request.POST["submit"] == "Rename":
+            return HttpResponse("The file Rename successfully!")
